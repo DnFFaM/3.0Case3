446c733f7f7272702be58652b13764c3c2586b37
adeaje <projectsmaker1803@outlook.com>
2022-09-21 09:07:29 +0200

    Add project files.

---
+++ 3.0Case3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._0Case3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 For Sign Up. ");
+            string one = Console.ReadLine();
+            Console.WriteLine("Press 2 For Login. ");
+            string two = Console.ReadLine();
+
+            if(one != null) {
+                Console.WriteLine(one);
+            }
+        }
+    }
+}
